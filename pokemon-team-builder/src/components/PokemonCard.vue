5098e4bdf33cb44bffa4f79f09eaf66d58636532
Daniel Navarro <daniel.navarro9697@gmail.com>
2024-08-15 20:35:04 -0600

    delete coments

--- pokemon-team-builder/src/components/PokemonCard.vue
+++ pokemon-team-builder/src/components/PokemonCard.vue
@@ -25,7 +25,7 @@
     } else {
       const fullPokemon = {
         ...props.pokemon,
-        id: props.pokemon.id, // Coloca el id real cuando lo tengas
+        id: props.pokemon.id,
         stats: {
           hp: 0,
           attack: 0,
